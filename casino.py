ab1ed018fc2af491ea2709a4d9806e5cfb6f9f50
kishan <kishan@localhost.localdomain>
2021-05-09 18:05:01 +0530

    try to make 3 players

--- casino.py
+++ casino.py
@@ -1,24 +1,45 @@
 import socket
 import threading
 import random
+import concurrent.futures
 
 HOST = "127.0.0.1"
-PORT = 1025
+PORT = 65432
+TOT_PLAYERS = 3
+DECK_SIZE = 52
+ROUNDS = 4
 
 
-def card_provider():
-    l = []
-    while len(l) != 3:
-        new_card = random.randrange(1, 53)
-        if new_card not in l:
-            l.append(new_card)
-    return str(l)
+def card_provider(l):
+    cards = []
+    while len(cards) != l:
+        new_card = random.randrange(1, DECK_SIZE+1)
+        if new_card not in cards:
+            cards.append(new_card)
+    return cards
 
+conn_list = []
+addr_list = []
+
+def game_manager(connections, addresses):
+    winner_list = []
+    counter = 1
+    while True:
+        dist_deck = card_provider
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
+    s.listen(TOT_PLAYERS)
+    for _ in range(TOT_PLAYERS):
+        conn, addr = s.accept()
+        conn_list.append(conn)
+        addr_list.append(addr)
+        print("connected to:", addr)
+    game_manager(conn_list, addr_list)
+    
+
+
+def clientmanager(conn):
     with conn:
         print("connected to ", conn)
         counter = 0
