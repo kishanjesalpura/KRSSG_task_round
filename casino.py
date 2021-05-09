a36d29b697eb284bdc8fe0d8d6dba272a1f6b8ec
kishan <kishan@localhost.localdomain>
2021-05-09 22:10:47 +0530

    final working

--- casino.py
+++ casino.py
@@ -8,6 +8,7 @@ PORT = 65432
 TOT_PLAYERS = 3
 DECK_SIZE = 52
 ROUNDS = 4
+CARDS_PER_PLAYER = 3
 
 
 def card_provider(l):
@@ -24,8 +25,62 @@ addr_list = []
 def game_manager(connections, addresses):
     winner_list = []
     counter = 1
+    max_idx = []
     while True:
-        dist_deck = card_provider
+        dist_deck = card_provider(CARDS_PER_PLAYER*TOT_PLAYERS)
+        dist_deck = [dist_deck[x*CARDS_PER_PLAYER:(x+1)*CARDS_PER_PLAYER] for x in range(TOT_PLAYERS)]
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            p_moves = executor.map(client_manager, conn_list, dist_deck)
+            max_val = 1
+            idx = 0
+            for val in p_moves:
+                print (val)
+                if val > max_val:
+                    max_idx = [chr(idx+ord('A'))]
+                    max_val = val
+                elif val == max_val:
+                    max_idx.append(chr(idx+ord('A')))
+                idx += 1
+        if len(max_idx)==1:
+            print("Player", max_idx[0], "wins round", counter)
+        else:
+            print("Tie betweeen players", end = ' ')
+            for plyr in max_idx:
+                print(plyr, end = ' ')
+            print("In round 1")
+        counter+=1
+        winner_list.append(max_idx)
+        if counter % ROUNDS == 0:
+            win_lst = [0,0,0,0]
+            winnrs = []
+            max_val=0
+            for rd_wnnrs in winner_list:
+                for wnnrs in rd_wnnrs:
+                    win_lst[int(ord(wnnrs)-ord('A'))] += 1
+            for idx in range(TOT_PLAYERS):
+                if win_lst[idx] > max_val:
+                    winnrs = [chr(idx+ord('A'))]
+                elif win_lst[idx] == max_val:
+                    winnrs.append(chr(idx+ord('A')))
+
+
+            if len(winnrs)==1:
+                msg = f"Player {winnrs[0]} wins the game"
+            else:
+                msg = "Tie betweeen players"
+                for plyr in winnrs:
+                    msg+=" "+plyr
+            print(msg)
+            for con in conn_list:
+                con.sendall(msg.encode('utf-8'))
+            c = input("Cycle completed!\nDo you want to continue?(y/n): ")
+            if c.strip().lower() != 'y':
+                break
+
+def client_manager(connection, dist_deck):
+    connection.sendall(str(dist_deck).encode('utf-8'))
+    card = connection.recv(1024)
+    return (int(card.decode('utf-8')))
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
@@ -36,21 +91,3 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         addr_list.append(addr)
         print("connected to:", addr)
     game_manager(conn_list, addr_list)
-    
-
-
-def clientmanager(conn):
-    with conn:
-        print("connected to ", conn)
-        counter = 0
-        while True:
-            counter += 1
-            cards = card_provider()
-            encoded_cards = cards.encode('utf-8')
-            conn.sendall(encoded_cards)
-            max_card = conn.recv(1024)
-            print("The recieved card is", max_card.decode('utf-8'))
-            if counter % 4 == 0:
-                c = input("Cycle completed!\nDo you want to continue?(y/n): ")
-                if c.lower() != 'y':
-                    break
--- client.py
+++ client.py
@@ -1,7 +1,9 @@
 import socket
 
 HOST = "127.0.0.1"
-PORT = 1025
+PORT = 65432
+
+ROUNDS = 4
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
@@ -10,13 +12,16 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         cards = cards.decode('utf-8')
         if len(cards)==0:
             break
-        cards = eval(cards)
-        print("cards recieved are: ", cards)
-        max_card = 1
-        for card in cards:
-            if card%13==0:
-                max_card = 13
-                break
-            elif card%13 > max_card:
-                max_card = card%13
-        s.sendall(str(max_card).encode('utf-8'))
+        try:
+            cards = eval(cards)
+            print("cards recieved are: ", cards)
+            max_card = 0
+            for card in cards:
+                if card%13==0:
+                    max_card = 13
+                    break
+                elif card%13 > max_card:
+                    max_card = card%13
+            s.sendall(str(max_card).encode('utf-8'))
+        except:
+            print(cards)
