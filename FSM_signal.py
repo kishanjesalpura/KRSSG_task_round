36d027f9a861594442c727a5bfc194349bfaff2f
kishan <kishan@localhost.localdomain>
2021-05-12 18:27:03 +0530

    restructured code of FSM signals

--- FSM_signal.py
+++ FSM_signal.py
@@ -10,70 +10,73 @@ possible_pairs = {
     'D1' : ['D2', 'B2', 'C1'],
     'D2' : ['A1', 'D1', 'C2']
 }
+t = None
 
-def input_and_modifier():
+
+def cars_arriving():
+    cars = input("Cars arrived : ")
+    counter = 0
+    for road in curr_values:
+        curr_values[road] += int(cars[counter])
+        counter += 1
+    return find_max_traffic
+
+
+def check_counter():
     global counter
-    counter -= 1
-    cars_arrived = list(input("\ncars arriving : ").strip())
-    y = 0
-    for x in curr_values.keys():
-        curr_values[x] += int(cars_arrived[y])
-        y+=1
-    print("\nCars waiting are : ", end="")
-    for x in curr_values.values():
-        print(x, end=" ")
-    print('\n')
+    counter += 1
+    if counter <= t:
+        return cars_arriving
+    elif sum(curr_values.values()) != 0:
+        return find_max_traffic
+    else:
+        print("Zero traffic left\nExiting...")
+        return None
 
 
-def max_signal(timecycle=1):
-    if counter>0:
-        input_and_modifier()
-    val = 0
-    name = []
-    for n in curr_values:
-        if curr_values[n]>val:
-            name = [n]
-            val = curr_values[n]
-        elif curr_values[n]==val:
-            name.append(n)
-    move_decider(name, timecycle)
+def find_max_traffic():
+    m_t_val = 0
+    for road in curr_values:
+        if curr_values[road]> m_t_val:
+            m_t_val = curr_values[road]
+            m_t_road = road
+    return [find_optimum_signals, m_t_road]
 
+def find_optimum_signals(m_t_road):
+    max_traffic = -1
+    for road2 in possible_pairs[m_t_road]:
+        if curr_values[road2] > max_traffic:
+            max_traffic = curr_values[road2]
+            m_t_road2 = road2
+    return [execute, [m_t_road, m_t_road2]]
 
-def move_decider(name_list, timecycle):
-    tot_val = 0
-    operation = []
-    for x in name_list:
-        for y in possible_pairs[x]:
-            if curr_values[x]+curr_values[y] > tot_val:
-                operation = [x, y]
-                tot_val = curr_values[x]+curr_values[y]
-    signal_printer(operation, timecycle)
-
-
-def signal_printer(operation, timecycle):
-    signals = {'A' : "A", 'B' : "B", 'C' : "C", 'D' : "D"}
-    msgs = {'1':" ; go straight", '2':" ; go right"}
+def execute(operation):
+    msgs = {'1':"go straight", '2':"go right"}
     operation = [x for x in operation if curr_values[x] != 0]
-    for x in operation:
-        curr_values[x] -= 1
-        signals[x[0]] += msgs[x[1]]
-    
-    print("Timecycle : ", timecycle)
-    for y in signals.values():
-        if len(y)==1:
-            print(y, "; off")
-        else:
-            print(y)
-    print("final values : ", end=' ')
-    cars_left = 0
-    for x in curr_values.values():
-        print(x, end=" ")
-        cars_left+=x
-    print('\n')
-    if cars_left!=0:
-        max_signal(timecycle+1)
-    
+    print(f"Time Step: {counter}")
+    print("signals to open fo this cycle are:")
+    print("all other signals are off.")
+    if len(operation)==2 and operation[0][0]==operation[1][0]:
+        print(f"{operation[0][0]} : {msgs['1']}, {msgs['2']}")
+    else:
+        for x in operation:
+            print(f"{x[0]} : {msgs[x[1]]}")
+    print("currently standing cars : ", end = ' ')
+    for x in curr_values:
+        if x in operation:
+            curr_values[x] -= 1
+        print(curr_values[x], end = ' ')
+    print()
+    return check_counter
+
 
 if __name__ == "__main__":
-    counter = int(input("Enter the number of inputs: "))
-    max_signal()
+    t = int(input("Enter t : "))
+    next = check_counter()
+    while True:
+        if next == None:
+            break
+        elif type(next) == list:
+            next = next[0](next[1])
+        else:
+            next = next()
