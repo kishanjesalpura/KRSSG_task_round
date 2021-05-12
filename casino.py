66a3b5caf52da6ec00367f24f533d03c24e026c3
kishan <kishan@localhost.localdomain>
2021-05-12 18:29:12 +0530

    version with proper comments and a slight structure change

--- casino.py
+++ casino.py
@@ -1,96 +1,157 @@
 import socket
 import threading
 import random
-import concurrent.futures
+from concurrent.futures import ThreadPoolExecutor
+from sys import exit
 
+# Some global variables used in the program
+
+# host IPv4 address and port number
 HOST = "127.0.0.1"
 PORT = 65432
+
+# Game related variables.
+# I have used these variables to make the program convenient to change.
 TOT_PLAYERS = 3
 DECK_SIZE = 52
-ROUNDS = 4
+rounds = 4
 CARDS_PER_PLAYER = 3
 
+# These variables store the players game and connection data
+conn_list = []
+addr_list = []
+win_counts = {}
+players = []
 
-def card_provider(l):
+# Makes a list of random cards.
+# The second part converts cards list strings(each string corresponds to one players cards)
+# The list of strings is the return value.
+def card_provider():
+
     cards = []
-    while len(cards) != l:
+    while len(cards) != TOT_PLAYERS*CARDS_PER_PLAYER:
         new_card = random.randrange(1, DECK_SIZE+1)
         if new_card not in cards:
             cards.append(new_card)
-    lst_to_return = []
+    playerwise_seperated = []
+
     for x in range(TOT_PLAYERS):
-        l = cards[x*CARDS_PER_PLAYER: (x+1)*CARDS_PER_PLAYER]
-        lst_to_return.append(str(l)[1:-1])
-    return lst_to_return
+        l = str(cards[x*CARDS_PER_PLAYER: (x+1)*CARDS_PER_PLAYER])
+        playerwise_seperated.append(l[1:-1])
+    return playerwise_seperated
 
-conn_list = []
-addr_list = []
-win_counts = {'A':0, 'B':0, 'C':0}
-players = ['A', 'B', 'C']
 
-def game_manager(connections, addresses):
+# Function keeps the track of the total number of rounds 
+# calls other fuctions and in short manages the game.
+def game_manager():
     round_counter = 1
-    while True:
-        dist_deck = card_provider(CARDS_PER_PLAYER*TOT_PLAYERS)
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            p_moves = executor.map(client_manager, conn_list, dist_deck)
-            max_val = 0
-            idx = 0
-            for val in p_moves:
-                if val > max_val:
-                    max_idx = [idx]
-                    max_val = val
-                elif val == max_val:
-                    max_idx.append(idx)
-                idx += 1
-        if len(max_idx)==1:
-            print("Player", players[max_idx[0]], "wins round", round_counter)
-            win_counts[players[max_idx[0]]] += 1
+
+    while round_counter!=0:
+        cards_for_players = card_provider()
+        
+        #ThreadPoolExecutor is fuction used to execute fuction in threads.
+        # Here map  fuction takes the lists and gives their corresponding elements to the fuction
+        # And runs that in different threads.
+        with ThreadPoolExecutor() as executor:
+            p_moves = executor.map(client_manager, conn_list, cards_for_players)
+        
+        max_card_val = 0
+        idx = 0
+        #finding the round winner(s) from all the returned cards
+        for player_max_card in p_moves:
+            if player_max_card > max_card_val:
+                max_idx_lst = [idx]
+                max_card_val = player_max_card
+            elif player_max_card == max_card_val:
+                max_idx_lst.append(idx)
+            idx += 1
+
+        # Printing message on server terminal
+        if len(max_idx_lst)==1:
+            print("Player", players[max_idx_lst[0]], "wins round", round_counter)
+            win_counts[players[max_idx_lst[0]]] += 1
         else:
             print("Tie betweeen players", end = ' ')
-            for plyr in max_idx:
-                print(players[plyr], end = ' ')
-                win_counts[players[plyr]] += 1
+            for idx in max_idx_lst:
+                print(players[idx], end = ' ')
+                win_counts[players[idx]] += 1
             print("In round", round_counter)
-        if round_counter % ROUNDS == 0:
-            max_val=0
-            for player in win_counts:
-                if win_counts[player] > max_val:
-                    winnrs = [player]
-                    max_val = win_counts[player]
-                elif win_counts[player] == max_val:
-                    winnrs.append(player)
+
+        if round_counter >= rounds:
+            # To ask player's consent after all rounds are finished
+            round_counter = printing_winners(round_counter)
+        else:
+            round_counter+=1
 
 
-            if len(winnrs)==1:
-                msg = f"Player {winnrs[0]} wins the game"
-            else:
-                msg = "Tie betweeen players"
-                for plyr in winnrs:
-                    msg+=" "+plyr
-            print(msg)
-            for con in conn_list:
-                con.sendall(msg.encode('utf-8'))
-            c = input("Cycle completed!\nDo you want to continue?(y/n): ")
-            if c.strip().lower() != 'y':
-                break
-            round_counter = 1
-            for x in win_counts:
-                win_counts[x] = 0
-            continue
-        round_counter+=1
+# Sending cards to the connection object given and recieving the response
+def client_manager(player_conn, cards_to_send):
+    player_conn.sendall(str(cards_to_send).encode('utf-8'))
+    card = player_conn.recv(1024).decode('utf-8')
+    return (int(card))
 
-def client_manager(connection, dist_deck):
-    connection.sendall(str(dist_deck).encode('utf-8'))
-    card = connection.recv(1024)
-    return (int(card.decode('utf-8')))
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen(TOT_PLAYERS)
-    for _ in range(TOT_PLAYERS):
-        conn, addr = s.accept()
-        conn_list.append(conn)
-        addr_list.append(addr)
-        print("connected to:", addr)
-    game_manager(conn_list, addr_list)
+def printing_winners(round_counter):
+    max_val=0
+    # Finding all the winners of the game and storing them in a list
+    for player in win_counts:
+        if win_counts[player] > max_val:
+            winnrs = [player]
+            max_val = win_counts[player]
+        elif win_counts[player] == max_val:
+            winnrs.append(player)
+
+    if len(winnrs)==1:
+        msg = f"Player {winnrs[0]} wins the game"
+    else:
+        msg = "Tie betweeen players"
+        for plyr in winnrs:
+            msg+=" "+plyr
+    print(msg)
+    msg+="\nCycle completed!\nDo you want to continue?(y/n): "
+
+    # Running thrreads to send the game winner message and asking wether they want to continue or not.
+    with ThreadPoolExecutor() as executor:
+        p_moves = executor.map(print_and_ask, conn_list, [msg]*TOT_PLAYERS)
+    for x in p_moves:
+        if not x:
+            return 0
+    return round_counter+1
+
+
+# Fuction used for threading in the above function
+def print_and_ask(conn, msg):
+    conn.sendall(msg.encode('utf-8'))
+    response = conn.recv(1024)
+    response = int(response.decode('utf-8'))
+    if response: 
+        return(1)
+    
+
+# main programme
+if __name__ == "__main__":
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        try:
+            s.bind((HOST, PORT))
+        except:
+            print(f"unable to bind to port {PORT}")
+            print("Please try again after some time")
+            exit(1)
+
+        # Connecting to all the players,
+        # and sending them their data and totol number of round.
+        s.listen(TOT_PLAYERS)
+        for a in range(TOT_PLAYERS):
+            conn, addr = s.accept()
+            conn_list.append(conn)
+            addr_list.append(addr)
+            print("connected to:", addr)
+
+            win_counts[str(a+1)] = 0
+            players.append(str(a+1))
+            player_data = str(a)+','+str(rounds)
+            conn.sendall(player_data.encode('utf-8'))
+
+        game_manager()
+
+    print("Game successfully completed.!")
--- client.py
+++ client.py
@@ -1,21 +1,42 @@
 import socket
+from sys import exit
 
 HOST = "127.0.0.1"
 PORT = 65432
-
-ROUNDS = 4
+    
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
+
+    # Trying to  establish a connection with the server
+    try:
+        s.connect((HOST, PORT))
+    except:
+        print(f'No server on port {PORT}')
+        print("Server not found")
+        print("Please chaeck wether the server program is running or not.")
+        print("Else try checking the port values in client and server")
+        exit(1)
+
+    print(f"Connected to the server : ('{HOST}', {PORT})")
+    player_data = s.recv(1024)
+    player_data = player_data.decode('utf-8').split(',')
+    ROUNDS = int(player_data[1])
+    print("You are player", player_data[0])
+
     counter = 0
     while True:
         data = s.recv(2048)
         if not data:
             break
         data = data.decode('utf-8')
-        if counter==ROUNDS:
+        if counter>=ROUNDS:
             print(data)
-            counter=0
+            x = input()
+            if x.strip().lower() == 'y':
+                s.sendall('1'.encode('utf-8'))
+            else:
+                s.sendall('0'.encode('utf-8'))
+            counter -= 1
         else:
             cards = list(data.split(', '))
             cards = [int(x) for x in cards]
@@ -25,9 +46,10 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                     temp.append(13)
                 else:
                     temp.append(x%13)
-            print("cards recieved are: ", temp)
             max_card = max(temp)
+            print(f"cards recieved are: {temp} ; card sent: {max_card}")
             s.sendall(str(max_card).encode('utf-8'))
             counter+=1
+print("Game completed!")
         
         
